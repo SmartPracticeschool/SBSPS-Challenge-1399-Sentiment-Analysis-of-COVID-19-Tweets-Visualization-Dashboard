426fda50558a57a0ec503f9e3534907e437cb241
Shubham Kondekar <kondekarshubham123@gmail.com>
2020-07-14 13:02:58 +0530

    [Project] Covid19 Sentimental Dashboard

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SBSPS-Challenge-1399-Sentiment-Analysis-of-COVID-19-Tweets-Visualization-Dashboard
+
+sudo /opt/lampp/lampp start
+https://vitux.com/how-to-install-xampp-on-your-ubuntu-18-04-lts-system/
--- app/home/routes.py
+++ app/home/routes.py
@@ -1,14 +1,13 @@
 # -*- encoding: utf-8 -*-
-"""
-License: MIT
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from app.home import blueprint
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for,make_response
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
+
+from random import random
+import json
+import time
 
 @blueprint.route('/index')
 @login_required
@@ -34,3 +33,65 @@ def route_template(template):
     
     except:
         return render_template('page-500.html'), 500
+
+
+@blueprint.route('/dataPiecha', methods=["GET", "POST"])
+def dataPie():
+    # Data Format
+    # [Positive, Netural, Negative]
+
+    Positive = 10
+    Netural = 10
+    Negative = 10
+
+    data = [Positive, Netural, Negative]
+
+    response = make_response(json.dumps(data))
+
+    response.content_type = 'application/json'
+
+    return response
+
+@blueprint.route('/data', methods=["GET", "POST"])
+def data():
+    # Data Format
+    # [TIME, Positive, Netural, Negative]
+
+    Positive = random() * 100
+    Netural = random() * 55
+    Negative = random() * 67
+
+    data = [time.mktime(time.gmtime()) * 1000, Positive, Netural, Negative]
+
+    response = make_response(json.dumps(data))
+
+    response.content_type = 'application/json'
+
+    return response
+
+@blueprint.route('/words', methods=["GET", "POST"])
+def wordsdata():
+
+    data = "My Name is Shubsdfghjkwhdc wudiwuddad ksd ham Kondekar Kondekar Kondekar"
+
+    response = make_response(json.dumps(data))
+
+    response.content_type = 'application/json'
+
+    return response
+
+@blueprint.route('/barchart',methods=["GET","POST"])
+def barchart():
+    # Positive = [ele1,ele2,ele3,ele4,ele5]
+    # Neutral = [ele1,ele2,ele3,ele4,ele5]
+    # Negative = [ele1,ele2,ele3,ele4,ele5]
+
+    # data = [Positive,Neutral,Negative]
+
+    data = [[3,2,3,4,5],[5,6,1,2,3],[1,5,9,8,3]]
+    
+    response = make_response(json.dumps(data))
+
+    response.content_type = 'application/json'
+
+    return response
